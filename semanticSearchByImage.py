80ee1ff02429de8eb4c1f4973b5cd328079ad2f8
FORCHA PEARL <forchapearl1@gmail.com>
2023-11-11 20:43:42 +0100

    Switched from markdown to pagebuilder

--- semanticSearchByImage.py
+++ semanticSearchByImage.py
@@ -1,14 +1,16 @@
-from taipy.gui import Markdown
+import taipy.gui.builder as tgb
+from taipy import Gui
 
 
 
-semanticSearchByImage = Markdown("""
+with tgb.Page() as semanticSearchByImage:
+    tgb.image("{image_item_path}")
+    tgb.file_selector("{searched_image_content}" ,label='Upload Item', on_action='submit_search_image_scenario' , extensions='.jpg,.png')
+    
+    tgb.image("{image_results_item}")
+    tgb.part(partial='{dynamic_content_image}')
 
-<|{image_item_name}|image|>
-<|{searched_image_content}|file_selector|label=Upload Item|on_action=submit_search_image_scenario|extensions=.jpg,.png|drop_message=Drop Message|>
-<|{image_results_item}|image|>
 
-""")
 
 def run(state):
     print("running sematic search on image")
